aa0751b7be0320bfddedaf0f9a43eb0e8093df37
ninelamericanel <ninelamericanel@gmail.com>
2022-03-31 16:49:01 +0300

    create moviesList component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,48 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
+
+import { Film } from '../../types/app';
+import { MoviesList } from '../MoviesList';
 
 type AppState = {
-
+  films: Film[];
 };
 
-interface AppProp {}
+interface AppProps {}
 
-export default class App extends Component<AppProp | AppState> {
+export default class App extends Component<AppProps | AppState> {
+  state = {
+    films: [
+      {
+        name: 'The way back',
+        id: '1',
+        genres: ['Action', 'Drama'],
+        realise: new Date('March 5, 2020'),
+        poster: 'poster',
+        description:
+          'A former basketball all-star, who has lost his wife and family foundation in a struggle with addiction attempts to regain his soul  and salvation by becoming the coach of a disparate ethnically mixed high ...',
+      },
+      {
+        name: 'The way back',
+        id: '1',
+        genres: ['Action', 'Drama'],
+        realise: new Date('March 5, 2020'),
+        poster: 'poster',
+        description:
+          'A former basketball all-star, who has lost his wife and family foundation in a struggle with addiction attempts to regain his soul  and salvation by becoming the coach of a disparate ethnically mixed high ...',
+      },
+      {
+        name: 'The way back',
+        id: '1',
+        genres: ['Action', 'Drama'],
+        realise: new Date('March 5, 2020'),
+        poster: 'poster',
+        description:
+          'A former basketball all-star, who has lost his wife and family foundation in a struggle with addiction attempts to regain his soul  and salvation by becoming the coach of a disparate ethnically mixed high ...',
+      },
+    ],
+  };
 
-    render() {
-        return <h1>Movies</h1>
-    }
+  render() {
+    return <MoviesList />;
+  }
 }
---
+++ src/components/MoviesList/MoviesList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface MoviesListProps {}
+
+const MoviesList: React.FC<MoviesListProps> = () => {
+  return <h1>sss</h1>;
+};
+
+export default MoviesList;
