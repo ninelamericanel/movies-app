b422bb951578305082c6e25a2ba31bcaf5a69c6f
ninelamericanel <ninelamericanel@gmail.com>
2022-04-01 12:24:35 +0300

    added reset style

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 
 import { App } from './components/App';
+import './reset.scss';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root')!);
 
