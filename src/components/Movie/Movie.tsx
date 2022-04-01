faeb53d5adc5e522057eef37cdd4415bb697107a
ninelamericanel <ninelamericanel@gmail.com>
2022-04-01 13:03:28 +0300

    create Movie

---
+++ src/components/Movie/Movie.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { MovieType } from '../../types/app';
+import './Movie.scss';
+
+interface MovieProps {
+  movie: MovieType;
+}
+
+const Movie: React.FC<MovieProps> = ({ movie }) => {
+  const { name, description, genres, poster } = movie;
+  const arrayGenres = genres.map((genre, id) => (
+    <p key={id} className="movie__genre">
+      {genre}
+    </p>
+  ));
+
+  return (
+    <>
+      <div className="movie__poster">
+        <img src={poster} title={name}></img>
+      </div>
+      <div className="movie__info">
+        <h5 className="movie__title">{name}</h5>
+        {/*<p className="movie__realise">{realise}</p>*/}
+        <div className="movie__genres">{arrayGenres}</div>
+        <p className="movie__description">{description}</p>
+      </div>
+    </>
+  );
+};
+
+export default Movie;
