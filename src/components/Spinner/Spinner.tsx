e2d9c38deb8689a7026724aa164216c4a751ce27
ninelamericanel <ninelamericanel@gmail.com>
2022-04-04 15:50:41 +0300

    modified styles for spinner container

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -6,10 +6,13 @@ interface SpinnerProps {}
 
 const Spinner: React.FC<SpinnerProps> = () => {
   return (
-    <div className="loadingio-spinner-eclipse-qka85v5szjk">
-      <div className="ldio-a323zksey1f">
-        <div></div>
+    <div className="spinner">
+      <div className="loadingio-spinner-eclipse-qka85v5szjk">
+        <div className="ldio-a323zksey1f">
+          <div></div>
+        </div>
       </div>
+      <p className="spinner__text">We are search movies...</p>
     </div>
   );
 };
