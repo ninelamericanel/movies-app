9407e613389d5da0f663572fde5dcf93137db006
ninelamericanel <ninelamericanel@gmail.com>
2022-04-04 20:34:11 +0300

    added spin component and modified style for spin

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-
 import './Spinner.scss';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 interface SpinnerProps {}
 
 const Spinner: React.FC<SpinnerProps> = () => {
+  const antIcon = <LoadingOutlined style={{ fontSize: 48 }} spin />;
   return (
     <div className="spinner">
-      <div className="loadingio-spinner-eclipse-qka85v5szjk">
-        <div className="ldio-a323zksey1f">
-          <div></div>
-        </div>
-      </div>
+      <Spin indicator={antIcon} />
       <p className="spinner__text">We are search movies...</p>
     </div>
   );
