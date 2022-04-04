c8f30003088d09cd3315343950235396e4eae780
ninelamericanel <ninelamericanel@gmail.com>
2022-04-04 11:28:17 +0300

    create variable for url

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,12 @@
 export default class MovieService {
   api_key = '856dc569092ab9cdd989b0d8a88b2a64';
 
+  url = 'https://api.themoviedb.org/3';
+
   getResponse = async (value) => {
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.api_key}&query=${value}`);
+    const response = await fetch(`${this.url}/search/movie?api_key=${this.api_key}&query=${value}`);
     if (!response.ok) {
-      throw new Error('Something wrong');
+      throw new Error('Something wrong happen');
     }
     return response.json();
   };
