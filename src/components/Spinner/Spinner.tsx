db987c2a6d8a9ee39176c2386ffe12f0410ad96a
ninelamericanel <ninelamericanel@gmail.com>
2022-04-04 15:25:20 +0300

    create spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Spinner.scss';
+
+interface SpinnerProps {}
+
+const Spinner: React.FC<SpinnerProps> = () => {
+  return (
+    <div className="loadingio-spinner-eclipse-qka85v5szjk">
+      <div className="ldio-a323zksey1f">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
