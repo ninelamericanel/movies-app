26da4554caa9a60258412eab097903cd0b49ed94
ninelamericanel <ninelamericanel@gmail.com>
2022-04-15 14:52:59 +0300

    create RatedMovies component

---
+++ src/components/RatedMovies/RatedMovies.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RatedMovies: React.FC = () => {
+  return <div>fkfk</div>;
+};
+
+export default RatedMovies;
---
+++ src/components/RatedMovies/index.ts
@@ -0,0 +1,1 @@
+export { default as RatedMovies } from './RatedMovies';
