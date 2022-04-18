354f07101916c2a6856f85dd6852e3b13fd12661
ninelamericanel <ninelamericanel@gmail.com>
2022-04-18 14:55:28 +0300

    modified working localStorage session and added functions for modify state of rating of movies

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,13 +4,14 @@ import { MoviesList } from 'components/MoviesList';
 import { Tabs } from 'components/Tabs';
 import { SearchInput } from 'components/SearchInput';
 import { RatedMovies } from 'components/RatedMovies';
-import { SetTabFunc, SetValueToSearchFunc } from 'types/app';
+import { RatedMoviesType, SetRateMoviesFunc, SetTabFunc, SetValueToSearchFunc } from 'types/app';
 
 import './App.scss';
 
 type AppState = {
   search: string;
   tab: string;
+  ratedMovies: RatedMoviesType[];
 };
 
 interface AppProps {}
@@ -19,10 +20,19 @@ export default class App extends Component<AppProps | AppState> {
   state: AppState = {
     search: '',
     tab: 'search',
+    ratedMovies: [],
   };
 
   componentDidMount() {
-    localStorage.setItem('myRatedMovies', JSON.stringify([]));
+    const movies = JSON.parse(localStorage.myRatedMovies || '[]');
+    localStorage.setItem('myRatedMovies', JSON.stringify(movies));
+  }
+
+  componentDidUpdate(prevProps: AppProps, prevState: AppState) {
+    const { ratedMovies } = this.state;
+    if (prevState.ratedMovies.length !== ratedMovies.length) {
+      localStorage.setItem('myRatedMovies', JSON.stringify(ratedMovies));
+    }
   }
 
   setValueToSearch: SetValueToSearchFunc = (value) => {
@@ -37,9 +47,15 @@ export default class App extends Component<AppProps | AppState> {
     });
   };
 
+  setRateMovies: SetRateMoviesFunc = (array) => {
+    this.setState({
+      ratedMovies: array,
+    });
+  };
+
   render() {
     const { search, tab } = this.state;
-    const movieListOutput = search ? <MoviesList search={search} /> : null;
+    const movieListOutput = search ? <MoviesList search={search} setRateMovies={this.setRateMovies} /> : null;
     const viewTab = tab === 'rated' ? <RatedMovies /> : movieListOutput;
     const viewSearchInput = tab === 'search' ? <SearchInput setValueToSearch={this.setValueToSearch} /> : null;
     return (
