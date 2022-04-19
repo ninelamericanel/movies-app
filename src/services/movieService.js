41de7b4d0d198de8068389fc3e49ecf8d5ce33cc
ninelamericanel <ninelamericanel@gmail.com>
2022-04-19 16:52:04 +0300

    added getting genres

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,7 +3,7 @@ export default class MovieService {
 
   url = 'https://api.themoviedb.org/3';
 
-  getResponse = async (value, page) => {
+  getResponseMovies = async (value, page) => {
     const response = await fetch(`${this.url}/search/movie?api_key=${this.api_key}&query=${value}&page=${page}`);
     if (!response.ok) {
       throw new Error(`Something wrong happen! Code of Error: ${response.status}`);
@@ -12,8 +12,23 @@ export default class MovieService {
     return response.json();
   };
 
+  getResponseGenres = async () => {
+    const response = await fetch(`${this.url}/genre/movie/list?api_key=${this.api_key}`);
+    if (!response.ok) {
+      throw new Error(`Something wrong happen! Code of Error: ${response.status}`);
+    }
+
+    return response.json();
+  };
+
+  getGenres = async () => {
+    return this.getResponseGenres()
+      .then((res) => res)
+      .catch((err) => err);
+  };
+
   getMovies = async (value, page) => {
-    return this.getResponse(value, page)
+    return this.getResponseMovies(value, page)
       .then((res) => [res.results, res.total_results])
       .catch((err) => err);
   };
